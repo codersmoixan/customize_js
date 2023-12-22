9bd61db3715a8c72adc49634f084aa3ae94c8453
Zhengji <zhengji.su@tmogroup.asia>
2023-12-22 16:56:45 +0800

    feat: 1. 解决Animate函数边界问题 2. 完成滚动文字动画效果

--- src/scrollTypography/animate.js
+++ src/scrollTypography/animate.js
@@ -1,23 +1,55 @@
 class Animation {
   animationMap = new Map()
+  animations = []
   from = {}
   to = {}
   delay = 0
+  isRunning = false
+  isCompleted = false
+  isBeforeStart = false
+  isAfterEnd = false
+  animationIndex = 0
+  state = []
 
-  constructor({ documents = [], start, end, from, to, animate, delay = 0 } = {}) {
+  constructor(documents, options) {
+    this.animations = Array.isArray(options) ? options : [options]
+    this.documents = documents
+
+    this.updateAnimationMaps(this.animations[this.animationIndex])
+
+    this.observer(['isBeforeStart', 'isAfterEnd'], newValue => {
+      console.log(newValue, 'newValue');
+      if (newValue.isAfterEnd && this.animationIndex < this.animations.length - 1) {
+        this.animationIndex += 1
+      }
+      if (newValue.isBeforeStart) {
+        this.animationIndex -= 1
+      }
+      this.updateAnimationMaps(this.animations[this.animationIndex])
+    })
+  }
+
+  updateAnimationMaps(option) {
+    const { start, end, from, to, animate, delay = 0 } = option
     this.delay = delay
     this.from = from
     this.to = to
     this.animate = animate
-    for (const dom of documents) {
-      this.animationMap.set(dom, this.getDomAnimation(start, end, dom))
-    }
+    this.documents.forEach((dom, index) => {
+      this.state[index] = {
+        running: false,
+        completed: false,
+        beforeStart: false,
+        afterEnd: false
+      }
+      this.animationMap.set(dom, this.getDomAnimation(start, end, dom, index))
+    })
   }
 
-  getAnimation(start, end) {
+  getAnimation(start, end, index) {
     const styles = {}
     for (const [key, value] of Object.entries(this.from)) {
-      const animation = this.create(start, end, value, this.to[key])
+      const animation = this.create(start, end, value, this.to[key], index)
       if (this.animate) {
         styles[key] = animation
       } else {
@@ -30,20 +62,43 @@ class Animation {
     return this.animate?.(styles) || styles
   }
 
-  getDomAnimation(start, end, dom) {
+  getDomAnimation(start, end, dom, index) {
     start += (dom.dataset.index || 0) * this.delay
-    return this.getAnimation(start, end, this.from, this.to)
+    return this.getAnimation(start, end, index)
   }
 
-  create(scrollStart, scrollEnd, startValue, endValue) {
-    return function(x) {
+  create(scrollStart, scrollEnd, startValue, endValue, index) {
+    return (x) => {
+      const { running, beforeStart, afterEnd } = this.state[index]
       if (x < scrollStart) {
+        if (running) {
+          this.state[index] = {
+            afterEnd,
+            beforeStart: true,
+            running: false,
+            completed: true,
+          }
+        }
         return startValue
       }
       if (x > scrollEnd) {
+        if (running) {
+          this.state[index] = {
+            beforeStart,
+            afterEnd: true,
+            running: false,
+            completed: true
+          }
+        }
         return endValue
       }
 
+      this.state[index] = {
+        running: true,
+        beforeStart: false,
+        afterEnd: false,
+        completed: false
+      }
       const progress = (x - scrollStart) / (scrollEnd - scrollStart)
       return startValue + (endValue - startValue) * progress
     }
@@ -52,9 +107,34 @@ class Animation {
   update(y) {
     for (const [dom, animations] of this.animationMap) {
       for (const prop in animations) {
-        const style = animations[prop](y)
         dom.style[prop] = animations[prop](y)
       }
     }
+
+    this.isCompleted = this.state.every(item => item.completed)
+    this.isBeforeStart = this.state.every(item => item.beforeStart)
+    this.isAfterEnd = this.state.every(item => item.afterEnd)
+    this.isRunning = this.state.some(item => item.running)
+    return this
+  }
+
+  observer(keys, callback) {
+    keys = Array.isArray(keys) ? keys : [keys]
+    const watchKeys = {}
+    keys.forEach(key => {
+      let newValue
+      Object.defineProperty(this, key, {
+        set: value => {
+          if (newValue !== value) {
+            newValue = value
+            watchKeys[key] = newValue
+            callback?.(watchKeys)
+          }
+        }
+      })
+    })
+  }
+  push(options) {
+    this.animations.push(options)
   }
 }
